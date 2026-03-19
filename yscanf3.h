bd41211254f3cef34a771903a538f4ce3c202742
Yalvox-PLUS <yuzhouhunter@outlook.com>
2026-03-19 22:19:46 +0800

    Update yscanf3.h

--- yscanf3.h
+++ yscanf3.h
@@ -2,7 +2,7 @@
  * @file yscanf3.h
  * @brief High-performance buffered input parser (EOF-correct version)
  * @author Yalvox PLUS Studio
- * @email yuzhouhunter@outlook.com
+ * @email yalvoxplus@outlook.com
  * @version 3.0
  */
 
